97a191f341bbd282518a0ad5bd42abc202e0d2e7
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-08-25 15:58:19 -0500

    sql update practice

---
+++ update_exercises.sql
@@ -0,0 +1,18 @@
+SELECT * FROM albums;
+UPDATE albums SET sales = sales * 10;
+SELECT * FROM albums;
+SELECT * FROM albums WHERE release_date < 1980;
+UPDATE albums SET release_date = release_date - 100 WHERE release_date < 1980;
+SELECT * FROM albums;
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+SELECT * FROM albums;
+
+
+
+
+# After each SELECT add an UPDATE statement to:
+# Make all the albums 10 times more popular (sales * 10)
+# Move all the albums before 1980 back to the 1800s.
+# Change 'Michael Jackson' to 'Peter Jackson'
+# Add SELECT statements after each UPDATE so you can see the results of your handiwork.
