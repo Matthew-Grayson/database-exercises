12aa14b263bce203e814c5e83528f144e7ba579e
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-08-25 13:34:00 -0500

    completed sql table creation

---
+++ albums_migration.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS albums;
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100),
+    name  VARCHAR(100),
+    release_date INT,
+    sales FLOAT,
+    genre VARCHAR(100),
+    PRIMARY KEY (id)
+);
