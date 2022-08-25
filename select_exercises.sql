d2bf2988b2186cc508a1ffe1604c3e3bf395681d
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-08-25 15:02:41 -0500

    completed sql seeder file

---
+++ select_exercises.sql
@@ -0,0 +1,3 @@
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club';
+SELECT genre FROM albums WHERE name = 'Rumours';
