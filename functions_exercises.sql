3a77692a17cc4967235e611bff74e3c4b48ac749
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-09-01 09:51:39 -0500

    sql concat and date methods practice

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -18,9 +18,19 @@ WHERE last_name LIKE ('E%') OR
         last_name LIKE ('%e')
 ORDER BY emp_no DESC;
 
-SELECT * FROM employees
+SELECT *, CONCAT(first_name, ' ', last_name) AS name FROM employees
 WHERE last_name LIKE ('E%') AND
         last_name LIKE ('%e');
+
+SELECT * FROM employees
+WHERE MONTH(birth_date) = 12 AND
+      DAY(birth_date) = 25;
+
+SELECT *, datediff(now(), hire_date) AS days_at_company FROM employees
+WHERE MONTH(birth_date) = 12 AND
+        DAY(birth_date) = 25 AND
+        YEAR(hire_date) BETWEEN 1990 AND 1999
+ORDER BY birth_date, hire_date DESC;
 
 SELECT * FROM employees
 WHERE last_name LIKE ('%q%') AND
